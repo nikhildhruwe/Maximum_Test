c97aedd26834f3d94c695b4f28aaf62c1374a2b4
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-21 19:25:12 +0530

    Refactored, Generic class and generic type variable

--- src/main/java/com/bridgelabz/maximum/utility/Maximum/Maximum.java
+++ src/main/java/com/bridgelabz/maximum/utility/Maximum/Maximum.java
@@ -1,6 +1,20 @@
 package com.bridgelabz.maximum.utility.Maximum;
 
-public class Maximum {
+public class Maximum <T extends Comparable >{
+
+    private T firstMember;
+    private T secondMember;
+    private T thirdMember;
+
+    public Maximum(T firstMember, T secondMember, T thirdMember) {
+        this.firstMember=firstMember;
+        this.secondMember=secondMember;
+        this.thirdMember=thirdMember;
+    }
+
+    public T maximumValue() {
+      return   Maximum.maximumValue(firstMember,secondMember,thirdMember);
+    }
 
     public static <T extends Comparable> T maximumValue(T firstMember, T secondMember, T thirdMember) {
         T maxValue=firstMember;
--- src/test/java/com/bridgelabz/maximumtesting/MaximumTest.java
+++ src/test/java/com/bridgelabz/maximumtesting/MaximumTest.java
@@ -8,65 +8,63 @@ public class MaximumTest {
 
     @Test
     public void givenMaximumNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
-        Maximum maximum = new Maximum();
-        int result = maximum.maximumValue(30,20,10);
+        Maximum maximum = new Maximum(30,20,10);
+        Comparable result = maximum.maximumValue();
         Assert.assertEquals(30, result);
     }
 
     @Test
     public void givenMaximumNumber_WhenASecondPosition_ShouldReturnSameNumber() {
-        Maximum maximum = new Maximum();
-        Integer result = maximum.maximumValue(30, 40, 10);
-        Assert.assertEquals((Integer) 40, result);
+        Maximum maximum = new Maximum(30, 40, 10);
+        Comparable result = maximum.maximumValue();
+        Assert.assertEquals( 40, result);
     }
 
     @Test
     public void givenMaximumNumber_WhenAThirdPosition_ShouldReturnSameNumber() {
-        Maximum maximum = new Maximum();
-        Integer result = maximum.maximumValue(30, 40, 50);
-        Assert.assertEquals((Integer) 50, result);
+        Maximum maximum = new Maximum(30, 40, 50);
+        Comparable result = maximum.maximumValue();
+        Assert.assertEquals( 50, result);
     }
 
     @Test
     public void givenMaximumFloatNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
-        Maximum maximum = new Maximum();
-        Float result= maximum.maximumValue(3.3f,2.2f,1.1f);
-        Assert.assertEquals((Float) 3.3f,result);
+        Maximum maximum = new Maximum(3.3f,2.2f,1.1f);
+        Comparable result= maximum.maximumValue();
+        Assert.assertEquals( 3.3f,result);
     }
 
     @Test
     public void givenMaximumFloatNumber_WhenAtSecondPosition_ShouldReturnSameNumber() {
-        Maximum maximum = new Maximum();
-        Float result= maximum.maximumValue(3.3f,20.6f,1.1f);
-        Assert.assertEquals((Float) 20.6f,result);
+        Maximum maximum = new Maximum(3.3f,20.6f,1.1f);
+        Comparable result= maximum.maximumValue();
+        Assert.assertEquals( 20.6f,result);
     }
     
     @Test
     public void givenMaximumFloatNumber_WhenAtThirdPosition_ShouldReturnSameNumber() {
-        Maximum maximum = new Maximum();
-        Float result= maximum.maximumValue(3.3f,20.6f,40.1f);
-        Assert.assertEquals((Float) 40.1f,result);
+        Maximum maximum = new Maximum(3.3f,20.6f,40.1f);
+        Comparable result= maximum.maximumValue();
+        Assert.assertEquals( 40.1f,result);
     }
 
     @Test
     public void givenMaximumStringValue_WhenAtFirstPosition_ShouldReturnSameString() {
-        Maximum maximum = new Maximum();
-        String result=maximum.maximumValue("PLANE","CAR","BIKE");
+        Maximum maximum = new Maximum("PLANE","CAR","BIKE");
+        Comparable result=maximum.maximumValue();
         Assert.assertEquals("PLANE",result);
     }
 
     @Test
     public void givenMaximumStringValue_WhenAtSecondPosition_ShouldReturnSameString() {
-        Maximum maximum = new Maximum();
-        String result=maximum.maximumValue("BIKE","PLANE","CAR");
+        Maximum maximum = new Maximum("BIKE","PLANE","CAR");
+        Comparable result=maximum.maximumValue();
         Assert.assertEquals("PLANE",result);
     }
     @Test
     public void givenMaximumStringValue_WhenAtThirdPosition_ShouldReturnSameString() {
-        Maximum maximum = new Maximum();
-        String result=maximum.maximumValue("BIKE","CAR","PLANE");
+        Maximum maximum = new Maximum("BIKE","CAR","PLANE");
+        Comparable result=maximum.maximumValue();
         Assert.assertEquals("PLANE",result);
     }
-
-
 }
