a8698deea610da9897c9795b9e9888b508b7c210
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-21 15:03:49 +0530

    Added, test case for max integer at second position

--- src/test/java/com/bridgelabz/maximumtesting/MaximumTest.java
+++ src/test/java/com/bridgelabz/maximumtesting/MaximumTest.java
@@ -12,4 +12,11 @@ public class MaximumTest {
         int result = maximum.maximumValue(30, 20, 10);
         Assert.assertEquals(30, result);
     }
+
+    @Test
+    public void givenMaximumNumber_WhenASecondPosition_ShouldReturnSameNumber() {
+        Maximum maximum = new Maximum();
+        Integer result = maximum.maximumValue(30, 40, 10);
+        Assert.assertEquals((Integer) 40, result);
+    }
 }
